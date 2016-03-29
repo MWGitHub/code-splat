08c6f28ab08cefc6597364d82f7b379af9798875
Mike <mwwmail@gmail.com>
2016-03-29 10:52:59 -0400

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/main.js',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
