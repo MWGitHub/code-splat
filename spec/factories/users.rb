be2451c1a91757a6c27aa08ffbbd65aeaa7f4efc
mike <mwwmail@gmail.com>
2016-04-07 11:10:41 -0400

    Update user factory to make collisions rarer

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     username {"#{Faker::Internet.user_name}_#{Faker::Internet.user_name}"}
     password 'password'
-    email { Faker::Internet.safe_email }
+    email { "#{Faker::Internet.user_name}_#{Faker::Internet.safe_email}" }
 
     factory :user_with_projects do
       after(:create) do |user, evaluator|
