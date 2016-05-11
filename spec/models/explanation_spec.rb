fd10a79f984515c2970efd03dcb48e9cadbcb8a6
Mike <mwwmail@gmail.com>
2016-05-11 16:41:18 -0400

    Add travis and text overlap test

--- spec/models/explanation_spec.rb
+++ spec/models/explanation_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Explanation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not allow overlapping explanations" do
+    explanation = Explanation.first
+    
+    expect do
+      Explanation.create!(
+        source_file_id: explanation.source_file_id,
+        fragment: explanation.fragment,
+        fragment_start: explanation.fragment_start,
+        fragment_end: explanation.fragment_end
+      )
+    end.to raise_error
+  end
 end
--- spec/models/text_change_spec.rb
+++ spec/models/text_change_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe Change, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe TextChange, type: :model do
 end
